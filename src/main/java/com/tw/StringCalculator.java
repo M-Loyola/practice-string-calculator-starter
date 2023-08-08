04e6186597a8710b4fd83ffcaef4a45300459bb4
m-loyola <m.yakit.loyola@gmail.com>
2023-08-08 16:26:04 +0800

    (JAVA-EXAM): Refactor code by using flatMapToInt for removing extra lines

--- src/main/java/com/tw/StringCalculator.java
+++ src/main/java/com/tw/StringCalculator.java
@@ -1,12 +1,12 @@
 package com.tw;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class StringCalculator {
     public int add(String string) {
         return string.isEmpty() ? 0 : Arrays.stream(string.split("[,\n;]|[^0-9]"))
-                .filter(s -> !s.isEmpty())
-                .mapToInt(Integer::parseInt)
+                .flatMapToInt(s -> !s.isEmpty() ? IntStream.of(Integer.parseInt(s)) : IntStream.empty())
                 .sum();
     }
 }
