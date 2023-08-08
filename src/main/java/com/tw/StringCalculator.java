cfc7f68747d6f20a7b16eb12b172ba0aa0fd7ce2
m-loyola <m.yakit.loyola@gmail.com>
2023-08-08 16:21:16 +0800

    (JAVA-EXAM): refactor and simplify add function

--- src/main/java/com/tw/StringCalculator.java
+++ src/main/java/com/tw/StringCalculator.java
@@ -1,16 +1,10 @@
 package com.tw;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 public class StringCalculator {
     public int add(String string) {
-        if (string.isEmpty()) {
-            return 0;
-        }
-        return Arrays.stream(string.split("[,\n;]"))
-                .map(s -> s.replaceAll("[^0-9]", ""))
+        return string.isEmpty() ? 0 : Arrays.stream(string.split("[,\n;]|[^0-9]"))
                 .filter(s -> !s.isEmpty())
                 .mapToInt(Integer::parseInt)
                 .sum();
