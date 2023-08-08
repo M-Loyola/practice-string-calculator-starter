d30acbbd124f60b78bc138f7c977fedae688d01e
m-loyola <m.yakit.loyola@gmail.com>
2023-08-08 13:14:36 +0800

    (JAVA-EXAM): add enhancement to add function calculation and handling

--- src/main/java/com/tw/StringCalculator.java
+++ src/main/java/com/tw/StringCalculator.java
@@ -1,7 +1,16 @@
 package com.tw;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class StringCalculator {
     public int add(String string) {
-        return 0;
+        if (string.isEmpty()) {
+            return 0;
+        }
+        String[] splitted = string.split(",");
+        int sum = Arrays.stream(splitted).mapToInt(Integer::parseInt).sum();
+        return sum;
     }
 }
